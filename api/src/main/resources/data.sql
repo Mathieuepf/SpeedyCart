3c8912620a0feb47af1939c1f5a79d6e6bf6e53b
Margaux Blondel <margaux.blondel@free.fr>
2024-05-06 20:34:48 +0200

    feat(api): ajout de toutes les tables à shema.sql

--- api/src/main/resources/data.sql
+++ api/src/main/resources/data.sql
@@ -1,5 +1,2 @@
-INSERT INTO product (product_id, name, unit_price, description, stock,active_from, deactivate_from, deactivated,  weight, sizes, for_adults, shop_id)
-VALUES (1, 'nom', 2, '', 1, null, null, null, 1, 1, 1, 1);
-
-INSERT INTO client (client_id, firstname, lastname, active_from, deactive_from, client_DOB)
+INSERT INTO client (id, firstname, lastname, active_since, disable_since, date_of_birth)
 VALUES (1, 'John', 'Doe', '2024-05-02', NULL, '1990-01-01');
