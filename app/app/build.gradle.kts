e6a1427c0ed0a503243d4e11912b74e50e4b48a3
Margaux Blondel <margaux.blondel@free.fr>
2024-05-06 17:18:36 +0200

    style(app): ajout de commentaires dans le build.gradle.kts

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 
 dependencies {
 
+    // Dépendances principales
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
@@ -46,21 +47,27 @@ dependencies {
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-    // bibliotheque - verifier quel sont toutes utile
+    // Déclaration de la version de Jetpack Compose
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
+    // Dépendances spécifiques à Jetpack Compose
+    implementation(libs.androidx.activity.compose)
+    //noinspection UseTomlInstead
+    implementation("androidx.compose.material3:material3")
+    //noinspection UseTomlInstead
+    implementation("androidx.compose.ui:ui")
+    //noinspection UseTomlInstead
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation(libs.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    //noinspection UseTomlInstead
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    //noinspection UseTomlInstead
     debugImplementation("androidx.compose.ui:ui-tooling")
+
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    // Retrofit with Scalar Converter
-    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+    implementation(libs.retrofit)
+    implementation(libs.retrofit2.converter.scalars)
 
 }
