504613205c56cc05e506b5601e819f124ebf2286
Mathieuepf <mathieu.roubaud@epfedu.fr>
2024-05-21 09:07:10 +0200

    Ajout activity_product_info

---
+++ app/app/src/main/java/fr/epf/min1/speedycart/activity_product_info.kt
@@ -0,0 +1,11 @@
+package fr.epf.min1.speedycart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_product_info : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_info)
+    }
+}
