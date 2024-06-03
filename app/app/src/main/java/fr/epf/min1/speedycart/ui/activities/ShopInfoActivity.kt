910a4489e97368ee203d7661c5dca0aeffb2bcdc
Mathieuepf <mathieu.roubaud@epfedu.fr>
2024-06-03 14:08:40 +0200

    Ajout du recyclerView pour products sur activity_shop_info

--- app/app/src/main/java/fr/epf/min1/speedycart/ui/activities/ShopInfoActivity.kt
+++ app/app/src/main/java/fr/epf/min1/speedycart/ui/activities/ShopInfoActivity.kt
@@ -2,6 +2,7 @@ package fr.epf.min1.speedycart.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import fr.epf.min1.speedycart.R
 
 class activity_shop_info : AppCompatActivity() {
@@ -9,4 +10,6 @@ class activity_shop_info : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_info)
     }
+
+    //val listProductRecView = findViewById<RecyclerView>(R.id.rec)
 }
