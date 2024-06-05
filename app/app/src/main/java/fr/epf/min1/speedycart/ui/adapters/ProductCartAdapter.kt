14cab87cee4b0ca21ba08458d639155287a7f0fa
LoicC <loloduvaucluse@gmail.com>
2024-06-05 15:06:20 +0200

    feat(app): Mise en place de la liste des produits dans l'écran de panier

---
+++ app/app/src/main/java/fr/epf/min1/speedycart/ui/adapters/ProductCartAdapter.kt
@@ -0,0 +1,60 @@
+package fr.epf.min1.speedycart.ui.adapters
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import fr.epf.min1.speedycart.R
+import fr.epf.min1.speedycart.data.Product
+import fr.epf.min1.speedycart.data.click
+import fr.epf.min1.speedycart.ui.activities.ClientAccountActivity
+import org.w3c.dom.Text
+
+class ProductCartViewHolder(item: View): RecyclerView.ViewHolder(item)
+
+const val PRODUCT_CART_EXTRA = "product"
+
+class ProductCartAdapter(val products: List<Product>): RecyclerView.Adapter<ProductCartViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductCartViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.product_cart_view, parent, false)
+        return ProductCartViewHolder(view)
+    }
+
+    override fun getItemCount() = products.size
+
+    override fun onBindViewHolder(holder: ProductCartViewHolder, position: Int) {
+        val product = products[position]
+        val view = holder.itemView
+
+        val nameTextView = view.findViewById<TextView>(R.id.name_product_cart_card_textview)
+        nameTextView.text = product.name
+
+        val priceTextView = view.findViewById<TextView>(R.id.price_product_cart_card_textview)
+        priceTextView.text = "${product.unitPrice}€"
+
+        val weightTextView = view.findViewById<TextView>(R.id.weight_product_cart_card_textview)
+        weightTextView.text = "${product.weight}g"
+
+        val shopNameTextView = view.findViewById<TextView>(R.id.shop_name_product_cart_card_textview)
+        shopNameTextView.text = product.shop.name
+
+        val productCartCard = view.findViewById<CardView>(R.id.product_cart_card_cardview)
+        productCartCard.click {
+            with(it.context){
+                val intent = Intent(this, ClientAccountActivity::class.java)
+                intent.putExtra(PRODUCT_CART_EXTRA, product)
+                startActivity(intent)
+            }
+        }
+
+        val deleteButton = view.findViewById<ImageButton>(R.id.delete_button_product_cart_card)
+        deleteButton.click {
+
+        }
+    }
+}
