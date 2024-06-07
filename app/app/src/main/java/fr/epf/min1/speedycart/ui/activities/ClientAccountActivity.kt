f9e3d5837350311ebf3408a44e22e87bffd87287
Margaux Blondel <margaux.blondel@free.fr>
2024-06-07 20:35:10 +0200

    feat(app-activity): Ajout activitÃe client fonctionnel

--- app/app/src/main/java/fr/epf/min1/speedycart/ui/activities/ClientAccountActivity.kt
+++ app/app/src/main/java/fr/epf/min1/speedycart/ui/activities/ClientAccountActivity.kt
@@ -1,15 +1,24 @@
 package fr.epf.min1.speedycart.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import fr.epf.min1.speedycart.MainActivity
 import fr.epf.min1.speedycart.R
-import fr.epf.min1.speedycart.data.Client
 import fr.epf.min1.speedycart.data.User
+import fr.epf.min1.speedycart.data.UserDTO
+import fr.epf.min1.speedycart.localstorage.AppRepository
 import fr.epf.min1.speedycart.network.Retrofit
 import fr.epf.min1.speedycart.network.SpeedyCartApiService
+import fr.epf.min1.speedycart.ui.fragments.NavigationBarFragment
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 private const val TAG = "ClientAccountActivity"
 
@@ -19,16 +28,70 @@ class ClientAccountActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client_account)
 
+        val repository = AppRepository(application)
+
+        lifecycleScope.launch {
+
+            // find user in local data base
+            val userDTO = findUserInLDB(repository)
+
+            // find user in remote database
+            val user = tryfetchClient(userDTO.id)
+
+            // show user Info
+            setUserInfoOnPage(user)
+
+            // user can logout
+            setLogoutButton(repository, userDTO)
+        }
+
+        // call navBar
+        if (savedInstanceState == null) { // check navbar not already created
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.client_account_navbar_fragment_container, NavigationBarFragment())
+                .commit()
+        }
+    }
+
+    private suspend fun findUserInLDB(repository: AppRepository): UserDTO {
+        val usersDTO = withContext(Dispatchers.IO) {
+            repository.getUser()
+        }
+        return usersDTO[0]
+    }
+
+    private fun setLogoutButton(repository: AppRepository, userDTO: UserDTO) {
+        val logoutButton = findViewById<ImageButton>(R.id.client_account_logout_imagebutton)
+        logoutButton.setOnClickListener {
+
+            lifecycleScope.launch {
+                withContext(Dispatchers.IO) {
+                    repository.deleteUser(userDTO)
+                }
+                val intent = Intent(this@ClientAccountActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun setUserInfoOnPage(user: User?) {
         val lastnameTextView = findViewById<TextView>(R.id.client_account_last_name)
         val firstnameTextView = findViewById<TextView>(R.id.client_account_first_name)
-
-        val user = tryfetchClient(1L)
+        val emailTextView = findViewById<TextView>(R.id.client_account_email)
+        val passwordTextView = findViewById<TextView>(R.id.client_account_password)
+        val dateOfBirthTextView = findViewById<TextView>(R.id.client_account_dateofbirth)
         if (user != null) {
             lastnameTextView.text = user.client!!.lastname
             firstnameTextView.text = user.client!!.firstname
+            emailTextView.text = user.mail
+            passwordTextView.text = user.password
+            dateOfBirthTextView.text = user.client!!.dateOfBirth.toString()
         } else {
             lastnameTextView.text = "error"
             firstnameTextView.text = "error"
+            emailTextView.text = "error"
+            passwordTextView.text = "error"
+            dateOfBirthTextView.text = "error"
         }
     }
 
