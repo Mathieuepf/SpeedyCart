c5a5e4d9f6606d978aff5666695f9b028d093090
LoicC <loloduvaucluse@gmail.com>
2024-06-08 17:38:50 +0200

    feat(app): Fonction de suppression des produits dans le panier

--- app/app/src/main/java/fr/epf/min1/speedycart/ui/adapters/ProductCartAdapter.kt
+++ app/app/src/main/java/fr/epf/min1/speedycart/ui/adapters/ProductCartAdapter.kt
@@ -1,23 +1,34 @@
 package fr.epf.min1.speedycart.ui.adapters
 
 import android.content.Intent
+import android.app.Activity
+import android.app.Application
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.lifecycle.findViewTreeLifecycleOwner
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import fr.epf.min1.speedycart.R
 import fr.epf.min1.speedycart.data.Product
 import fr.epf.min1.speedycart.data.ProductDTO
 import fr.epf.min1.speedycart.data.click
+import fr.epf.min1.speedycart.localstorage.AppRepository
 import fr.epf.min1.speedycart.ui.activities.ClientAccountActivity
+import fr.epf.min1.speedycart.ui.activities.ShopCartActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.jetbrains.skia.impl.Log
 import org.w3c.dom.Text
 
 class ProductCartViewHolder(item: View): RecyclerView.ViewHolder(item)
 
 const val PRODUCT_CART_EXTRA = "product"
+const val DELETE_CART_EXTRA = "todelete"
 
 class ProductCartAdapter(val products: List<ProductDTO>): RecyclerView.Adapter<ProductCartViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductCartViewHolder {
@@ -55,7 +66,11 @@ class ProductCartAdapter(val products: List<ProductDTO>): RecyclerView.Adapter<P
 
         val deleteButton = view.findViewById<ImageButton>(R.id.delete_button_product_cart_card)
         deleteButton.click {
-
+            with(it.context){
+                val intent = Intent(this, ShopCartActivity::class.java)
+                intent.putExtra(DELETE_CART_EXTRA, product)
+                startActivity(intent)
+            }
         }
     }
 }
