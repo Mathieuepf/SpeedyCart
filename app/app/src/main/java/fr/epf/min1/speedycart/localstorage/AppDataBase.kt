75f8af6d82ce2f23735908c5aeb3ea672243fcdb
LoicC <loloduvaucluse@gmail.com>
2024-06-08 16:16:47 +0200

    feat(app): Fonction d'ajout d'un produit au panier

--- app/app/src/main/java/fr/epf/min1/speedycart/localstorage/AppDataBase.kt
+++ app/app/src/main/java/fr/epf/min1/speedycart/localstorage/AppDataBase.kt
@@ -1,10 +1,19 @@
 package fr.epf.min1.speedycart.localstorage
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import fr.epf.min1.speedycart.data.ProductDTO
 import fr.epf.min1.speedycart.data.UserDTO
 
-@Database(entities = [UserDTO::class], version = 1, exportSchema = false)
+//@Database(entities = [UserDTO::class], version = 1, exportSchema = true)
+//abstract class AppDataBase : RoomDatabase() {
+//    abstract fun userDao(): UserDao
+//}
+
+@Database(entities = [UserDTO::class, ProductDTO::class], version = 2, autoMigrations = [AutoMigration(from = 1, to = 2)], exportSchema = true)
 abstract class AppDataBase : RoomDatabase() {
     abstract fun userDao(): UserDao
+
+    abstract fun productDao(): ProductDao
 }
