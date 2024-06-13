512b7d743891d3c7b711c421565b65aaa51b8f9a
Margaux Blondel <margaux.blondel@free.fr>
2024-06-13 18:37:52 +0200

    Ãfeat(api-controller): ajout POST/order endpoint

--- api/src/main/java/fr/epf/speedycart/api/controller/OrderController.java
+++ api/src/main/java/fr/epf/speedycart/api/controller/OrderController.java
@@ -1,11 +1,14 @@
 package fr.epf.speedycart.api.controller;
 
+import fr.epf.speedycart.api.model.Order;
 import fr.epf.speedycart.api.model.OrderDTO;
 import fr.epf.speedycart.api.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -16,5 +19,21 @@ public class OrderController {
     @GetMapping("/orders")
     public List<OrderDTO> getListOrders() {
         return orderService.getOrdersData();
+    }
+
+    @GetMapping("/order/{id}")
+    public OrderDTO getOrderById(@PathVariable long id) {
+        return orderService.getOrderData(id);
+    }
+
+    @PostMapping("/order")
+    public ResponseEntity<Order> saveProduct(@RequestBody OrderDTO orderDTO) {
+        Order orderAdded = orderService.saveOrderData(orderDTO);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(orderAdded.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
