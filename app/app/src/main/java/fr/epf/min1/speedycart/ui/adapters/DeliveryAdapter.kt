57fe76e859fd9f2d19eae08d7e8ab3b2537bef85
LoicC <loloduvaucluse@gmail.com>
2024-06-13 17:40:57 +0200

    feat(app): Mise en place front-end de la page de détail d'une livraison

--- app/app/src/main/java/fr/epf/min1/speedycart/ui/adapters/DeliveryAdapter.kt
+++ app/app/src/main/java/fr/epf/min1/speedycart/ui/adapters/DeliveryAdapter.kt
@@ -1,12 +1,16 @@
 package fr.epf.min1.speedycart.ui.adapters
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import fr.epf.min1.speedycart.R
 import fr.epf.min1.speedycart.data.Delivery
+import fr.epf.min1.speedycart.data.click
+import fr.epf.min1.speedycart.ui.activities.DeliveryDetailsActivity
 
 class DeliveryViewHolder(item: View): RecyclerView.ViewHolder(item)
 
@@ -31,5 +35,14 @@ class DeliveryAdapter(val deliveries: List<Delivery>): RecyclerView.Adapter<Deli
 
         val productNumberTextView = view.findViewById<TextView>(R.id.delivery_card_product_number_textview)
         productNumberTextView.text = delivery.fee.toString()
+
+        val deliveryCard = view.findViewById<CardView>(R.id.delivery_card_cardview)
+        deliveryCard.click {
+            with(it.context){
+                val intent = Intent(this, DeliveryDetailsActivity::class.java)
+                intent.putExtra(DELIVERY_EXTRA, delivery)
+                startActivity(intent)
+            }
+        }
     }
 }
